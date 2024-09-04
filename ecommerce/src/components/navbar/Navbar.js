f2fdac0d68593f05bc93e2cfbe10bae19dec4a1f
Májai Henrietta - Zita <majai.henrietta@student.ms.sapientia.ro>
2024-09-04 17:52:14 +0300

    Added search input to navbar

--- ecommerce/src/components/navbar/Navbar.js
+++ ecommerce/src/components/navbar/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Navbar.module.css'; 
 import Button from '../button/Button'; 
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
 
   const handleNavigate = (path) => {
     navigate(path); 
+  };
+
+  const toggleSearch = () => {
+    setIsSearchVisible(!isSearchVisible);
   };
 
   return (
@@ -21,9 +26,12 @@ const Navbar = () => {
         <li><Button variant="link" size="medium" onClick={() => handleNavigate('/products')}>Products</Button></li>
       </ul>
       <div className={styles.icons}>
-        <div className={styles.icon}>
+        <div className={styles.icon} onClick={toggleSearch} style={{ cursor: 'pointer' }}>
           <img src="/search-icon.svg" alt="Search" />
         </div>
+        {isSearchVisible && (
+          <input type="text" className={styles.searchInput} placeholder="Search..." />
+          )}
         <div className={styles.icon} onClick={() => handleNavigate('/login')} style={{ cursor: 'pointer' }}>
           <img src="/user-icon.svg" alt="Account" />
         </div>
